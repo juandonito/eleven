c3d9b5e84eb24871ccea073bf9d49778f75efcde
JuanDonito <anguadir@gmail.com>
2024-10-22 22:47:22 +0200

    chore: made HUDInput only controlled Input

--- frontend/src/components/HUDInput/HUDInput.tsx
+++ frontend/src/components/HUDInput/HUDInput.tsx
@@ -1,5 +1,5 @@
 // React
-import { forwardRef, InputHTMLAttributes } from 'react';
+import { InputHTMLAttributes } from 'react';
 
 // Libs
 import classnames from 'classnames';
@@ -8,60 +8,52 @@ import classnames from 'classnames';
 import styles from './HUDInput.module.css';
 
 interface HUDInputProps extends InputHTMLAttributes<HTMLInputElement> {
-  error?: string;
+  error?: string | null;
   label: string;
 }
 
-export const HUDInput = forwardRef<HTMLInputElement | null, HUDInputProps>(
-  function HUDInputComponent(
-    {
-      className,
-      defaultValue,
-      error = null,
-      label,
-      name,
-      onChange,
-      placeholder,
-      required = false,
-      style,
-      type = 'text',
-      value,
-    },
-    ref,
-  ) {
-    const componentClassNames = classnames(styles.hudinput, className);
-    const inputClassNames = classnames(styles.hudinputHtmlInput, {
-      [styles.hudinputHtmlInputError]: error !== null,
-    });
-    const labelClassNames = classnames(styles.hudinputLabel, {
-      [styles.hudinputLabelError]: error !== null,
-    });
-    const barClassNames = classnames(styles.hudinputBar, {
-      [styles.hudinputBarError]: error !== null,
-    });
+export function HUDInput({
+  className,
+  error = null,
+  label,
+  name,
+  onChange,
+  placeholder,
+  required = false,
+  style,
+  type = 'text',
+  value,
+}: HUDInputProps) {
+  const componentClassNames = classnames(styles.hudinput, className);
+  const inputClassNames = classnames(styles.hudinputHtmlInput, {
+    [styles.hudinputHtmlInputError]: error !== null,
+  });
+  const labelClassNames = classnames(styles.hudinputLabel, {
+    [styles.hudinputLabelError]: error !== null,
+  });
+  const barClassNames = classnames(styles.hudinputBar, {
+    [styles.hudinputBarError]: error !== null,
+  });
 
-    console.log('INPUT STATE RELOAD');
+  console.log('INPUT STATE RELOAD');
 
-    return (
-      <div className={componentClassNames} style={style}>
-        <input
-          id={name}
-          name={name}
-          type={type}
-          required={required}
-          className={inputClassNames}
-          placeholder={placeholder}
-          value={value}
-          onChange={onChange}
-          ref={ref}
-          defaultValue={defaultValue}
-        />
-        <label htmlFor={name} className={labelClassNames}>
-          {label}
-        </label>
-        <i className={barClassNames}></i>
-        {error && <div className={styles.hudinputError}>{error}</div>}
-      </div>
-    );
-  },
-);
+  return (
+    <div className={componentClassNames} style={style}>
+      <input
+        id={name}
+        name={name}
+        type={type}
+        required={required}
+        className={inputClassNames}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+      <label htmlFor={name} className={labelClassNames}>
+        {label}
+      </label>
+      <i className={barClassNames}></i>
+      {error && <div className={styles.hudinputError}>{error}</div>}
+    </div>
+  );
+}
